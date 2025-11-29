51d769fa6b50b563f5458c7d1fe3d7679d9092c5
rBaun <runeb95@gmail.com>
2025-11-29 22:39:36 +0100

    Moved registration check to invite component

--- runiko-wedding-invitation/src/app/components/invite/invite.component.ts
+++ runiko-wedding-invitation/src/app/components/invite/invite.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ButtonNextComponent } from '@components/shared/button-next/button-next.component';
 import { TranslocoModule } from '@jsverse/transloco';
+import { GoogleSheetsService } from '@services/google-sheets.service';
+import { HouseholdService } from '@services/household.service';
 import { ImagePortraitComponent } from './image-portrait/image-portrait.component';
 import { LocationInfoComponent } from './location-info/location-info.component';
 
@@ -19,7 +21,10 @@ export class InviteComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-  ) { }
+    private googleService: GoogleSheetsService,
+    private householdService: HouseholdService,
+  ) {
+  }
 
   protected onRsvpButtonClick = (): void => {
     this.router.navigateByUrl('/rsvp');
@@ -30,7 +35,10 @@ export class InviteComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => this.checkForInvitationCode(params));
+    this.route.params.subscribe(params => {
+      this.checkForInvitationCode(params);
+      this.checkForRegistration();
+    });
   }
 
   private checkForInvitationCode = (params: Params): void => {
@@ -42,7 +50,23 @@ export class InviteComponent implements OnInit {
     }
 
     this.isInvited = !!invitationCode;
-    this.isRegistered = localStorage.getItem('hasRegistered') === 'true';
+  }
+
+  private checkForRegistration = (): void => {
+    if (!this.isInvited) return;
+    if (this.isRegistered) return;
+
+    const isRegistered = localStorage.getItem('isRegistered') === 'true';
+    if (isRegistered) {
+      this.isRegistered = isRegistered;
+      return;
+    }
+
+    const names: string[] = this.householdService.getRsvpInfo()?.guests.map(guest => guest.name) ?? [];
+    this.googleService.read(names).subscribe(response => {
+      this.isRegistered = !!response.found;
+      localStorage.setItem('isRegistered', 'true');
+    })
   }
 
 }
